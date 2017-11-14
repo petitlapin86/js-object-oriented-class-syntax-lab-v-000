bd11738cc9163589ea5118e217088f74ad6d9a8e
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-14 13:28:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ releasePressStatement() {
   return "You will see great things from Scuber.";
 }
 sayHi() {
-  return "Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.";
+  return ("Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.");
 }
 }
 
