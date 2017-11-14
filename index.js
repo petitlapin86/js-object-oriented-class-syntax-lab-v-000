a04b6153207026e4bf02bbdb96d8fe578cb90146
Paige Skinner <skinnerpa86@yahoo.com>
2017-11-14 13:32:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ doCharity() {
 releasePressStatement() {
   return "You will see great things from Scuber.";
 }
-sayHi() {
-  return 'Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.'
-}
-}
+sayHi(){
+   return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+ }
+} 
 
 class Ceo extends BoardMember {
 hireEmployee(){
